5ac02ed6108f34d00547a09e7549088edccc5328
Giulia Colombo <giulia.colombo.127@gmail.com>
2024-10-25 18:25:34 +0200

    feat(views): update index with button_to

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,9 @@
 class OrdersController < ApplicationController
   # Create a new order with total_quantity and total_value of 0 to start.
   def create
+    Rails.logger.info("calling OrdersController.create()")
     @order = Order.new(total_quantity: 0, total_value: 0)
+    Rails.logger.debug("Newly created empty order #{@order.inspect()}")
    if @order.save()
      redirect_to new_order_item_path(@order), notice: "Order successfully started."
    else
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,15 +4,13 @@ class Order < ApplicationRecord
   validates :total_value, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :total_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_t0: 0 }
 
-  # def alculate_total
-  # 
-  def add_products_to_order(quantity):
-    # Adds products to the order based on the quantity the user has input.
-    # 
-    # for every product in the table (coffee, green tea, strawb) check if the user has input a number under the quantity columng
-    # if the quantity field is not empty, 
-  end
-    
-  end
-  
+  # # def alculate_total
+  # #
+  # def add_products_to_order(quantity):
+  #   # Adds products to the order based on the quantity the user has input.
+  #   #
+  #   # for every product in the table (coffee, green tea, strawb) check if the user has input a number under the quantity columng
+  #   # if the quantity field is not empty,
+  # end
+  # end
 end
